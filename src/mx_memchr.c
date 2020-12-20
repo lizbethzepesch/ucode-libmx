2e421c5f0ce1b246c4bb643e1704536c9e69b446
lizbethzepesch <elizbetti15@gmail.com>
2020-12-20 20:00:43 +0200

    d

---
+++ src/mx_memchr.c
@@ -0,0 +1,14 @@
+#include "libmx.h"
+
+void *mx_memchr(const void *s, int c, size_t n){
+
+    unsigned char *arr = (unsigned char *)s;
+
+    for (int i = 0; i < n; i++, s++){
+        if(*arr == (unsigned char) c)
+            return arr;
+    }
+    
+
+    return NULL;
+}
